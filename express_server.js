acf9a36dcaf1b2dca9076d3a88aa854ce9834acc
Casey <caseyjtite@gmail.com>
2022-01-24 13:25:25 -0800

    add server logic

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+app.get("/", (req, resp) => {
+  resp.send("Hello!, Is there anybody out there?");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
