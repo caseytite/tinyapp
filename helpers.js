91b97af30bba9c374dd7ac7b1dc7a4f9682fe82d
Casey <caseyjtite@gmail.com>
2022-02-27 17:02:59 -0800

    add/remove comments

--- helpers.js
+++ helpers.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 
+// Generates a random string to be used as a short URL link
+
 function generateRandomString() {
   let output = '';
   let randoms = '123456789abcdefghi';
@@ -9,6 +11,9 @@ function generateRandomString() {
 
   return output;
 }
+
+// Searches for a a user in the dataset by email
+
 function getUserByEmail(newEmail, users) {
   for (let user in users) {
     if (newEmail == users[user].email) {
@@ -18,8 +23,9 @@ function getUserByEmail(newEmail, users) {
   return undefined;
 }
 
+// Validates a users email and password when logging in
+
 const validateUser = function (email, hashed, password, users) {
-  //
   for (let user in users) {
     if (users[user].email === email && bcrypt.compareSync(password, hashed)) {
       return users[user];
@@ -27,6 +33,8 @@ const validateUser = function (email, hashed, password, users) {
   }
   return false;
 };
+
+// Searches the dataset and grabs the users personal urls
 
 const urlsForUser = function (userLoggedIn, urlDatabase) {
   const userUrls = {};
